00e8c4739474e0f98f96cdc1e5eb096fe3be6853
Guillaume Dorce <polynux.dev@gmail.com>
2024-03-24 18:13:07 +0100

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
 import tailwind from '@astrojs/tailwind';
-
 import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), tailwind(), solidJs()]
+  integrations: [db(), tailwind(), solidJs()],
+  output: "server",
+  adapter: vercel()
 });
